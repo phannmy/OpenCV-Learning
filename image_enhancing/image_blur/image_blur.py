63effd35e80f886ed1a0bb4b9451861a8bce6f31
my phan <phanmy24122007@gmail.com>
2023-07-15 21:39:36 +0700

    finishing study about image bluring

--- image_enhancing/image_blur/image_blur.py
+++ image_enhancing/image_blur/image_blur.py
@@ -4,3 +4,15 @@ import cv2
 a = cv2.imread('./img/RawData.jpg')
 cv2.imshow('img', a)
 cv2.waitKey()
+
+Gaussian = cv2.GaussianBlur(a, (7,7), 0)
+cv2.imshow("Gaussian Blur", Gaussian)
+cv2.waitKey()
+
+median = cv2.medianBlur(a, 5)
+cv2.imshow("Median Blur", median)   
+cv2.waitKey()
+
+bilateral = cv2.bilateralFilter(a, 9, 75, 75)
+cv2.imshow("Bilateral Blur", bilateral)
+cv2.waitKey()
